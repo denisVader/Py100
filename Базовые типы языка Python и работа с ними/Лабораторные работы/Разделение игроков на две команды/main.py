550b11c59e51cd45a8fac9f0f2438f04d44d68c1
DENVALI <vadera@mail.ru>
2023-10-19 21:10:39 +0300

    commit

--- Базовые типы языка Python и работа с ними/Лабораторные работы/Разделение игроков на две команды/main.py
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Разделение игроков на две команды/main.py
@@ -1,3 +1,10 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
-# TODO Разделите участников на две команды
+# Разделить участников на две команды.
+middle_value = len(list_players) // 2
+left_team = list_players[:middle_value]
+right_team = list_players[middle_value:]
+
+# Вывести на экран
+print(left_team)
+print(right_team)
--- Базовые типы языка Python и работа с ними/Лабораторные работы/Статистика посещений сайта/main.py
+++ Базовые типы языка Python и работа с ними/Лабораторные работы/Статистика посещений сайта/main.py
@@ -1,3 +1,6 @@
 users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
 
-# TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
+general = len(users)
+unique = len(set(users))
+
+print({"Общее количество": general, "Уникальные посещения": unique})
