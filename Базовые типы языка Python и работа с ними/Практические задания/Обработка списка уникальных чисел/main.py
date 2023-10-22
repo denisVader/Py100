68be3369c87931d9961d795243fd28eef9226a46
DENVALI <vadera@mail.ru>
2023-10-22 13:53:53 +0300

    commit

--- Базовые типы языка Python и работа с ними/Практические задания/Обработка списка уникальных чисел/main.py
+++ Базовые типы языка Python и работа с ними/Практические задания/Обработка списка уникальных чисел/main.py
@@ -1,7 +1,14 @@
 list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
 
 # TODO найти сумму, количество и среднее арифметическое уникальных чисел
+numbers = set(list_)
+sum_of_numbers = sum(numbers)
+count_of_numbers = len(numbers)
+average = sum_of_numbers / count_of_numbers
+a = 'Сумма уникальных чисел:'
+b = 'Количество уникальных чисел:'
+c = 'Среднее арифметическое уникальных чисел:'
+describe = {a:sum_of_numbers,b:count_of_numbers,c:average}
+print(describe)
 
-print("Сумма уникальных чисел:", ...)
-print("Количество уникальных чисел:", ...)
-print("Среднее арифметическое уникальных чисел:", ...)
+
